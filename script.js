36e21d7000ab4adea353345687e1b76694a8ac9d
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-01-30 18:43:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-require([
-  "esri/Map",
-  "esri/views/MapView",
-  "esri/layers/FeatureLayer"
-], function(Map, MapView, FeatureLayer) {
-    var map = new Map({
-        basemap: "hybrid"
+require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], (Map, MapView, FeatureLayer) => {
+    const map = new Map({
+        basemap: "dark-gray"
     });
 
-    var view = new MapView({
+    const view = new MapView({
         container: "viewDiv",
         map: map,
-        zoom: 4, 
-        center: [-98, 40] 
+        extent: {
+            xmin: -9177811,
+            ymin: 4247000,
+            xmax: -9176791,
+            ymax: 4247784,
+            spatialReference: 102100
+        }
     });
 
-    // Add feature layer
-    var featureLayer = new FeatureLayer({
-   const featureLayer = new FeatureLayer({
+    /********************
+     * Add feature layers
+     ********************/
+    const featureLayer1 = new FeatureLayer({
         url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Major_Cities_/FeatureServer/0"
     });
 
-    map.add(featureLayer);
+    const featureLayer2 = new FeatureLayer({
+        url: "https://services2.arcgis.com/I9cUOJUZvdGAJncI/arcgis/rest/services/Find_Locations_in_USA_Major_Cities/FeatureServer/0"
+    });
+
+    map.add(featureLayer1);
+    map.add(featureLayer2);
 });
