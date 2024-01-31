0d8e9f7c77946d33e0cfd9b72ede7fd76a8edece
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-01-30 18:21:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,17 @@ require([
 
     // Add feature layer
     var featureLayer = new FeatureLayer({
-    const featureLayer = new FeatureLayer({
+    const featureLayer1 = new FeatureLayer({
       portalItem: {
         id: "630750de135f4a7fbd2acfc257ae0e2f" 
       }
     });
-    map.add(featureLayer);
+    map.add(featureLayer1);
+   const featureLayer2 = new FeatureLayer({
+      portalItem: {
+        id: "8c2d6d7df8fa4142b0a1211c8dd66903"
+      }
+    });
+    map.add(featureLayer2);
+});
 
