a421ff03bc945a40f550e5b8cf4166d2ebff0900
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-01-30 18:24:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,22 +11,14 @@ require([
         container: "viewDiv",
         map: map,
         zoom: 4, 
-        center: [-98.5795, 39.82839] 
+        center: [-98, 40] 
     });
 
     // Add feature layer
     var featureLayer = new FeatureLayer({
-    const featureLayer1 = new FeatureLayer({
-      portalItem: {
-        id: "630750de135f4a7fbd2acfc257ae0e2f" 
-      }
+   const featureLayer = new FeatureLayer({
+        url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Major_Cities_/FeatureServer/0"
     });
-    map.add(featureLayer1);
-   const featureLayer2 = new FeatureLayer({
-      portalItem: {
-        id: "8c2d6d7df8fa4142b0a1211c8dd66903"
-      }
-    });
-    map.add(featureLayer2);
-});
 
+    map.add(featureLayer);
+});
